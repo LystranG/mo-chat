3df132d9b17e7f7ecf77e5f175e532761ba6462d
LystranG <me@lystran.com>
2026-03-04 22:04:43 +0800

    chore(native): add GraalVM native image build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     application
+    id("org.graalvm.buildtools.native") version "0.11.1"
 }
 
 dependencies {
     annotationProcessor("io.micronaut:micronaut-inject-java:4.9.0")
+    annotationProcessor("io.micronaut:micronaut-graal:4.9.0")
     implementation("io.micronaut:micronaut-runtime:4.9.0")
     runtimeOnly("ch.qos.logback:logback-classic:1.5.18")
     runtimeOnly("org.yaml:snakeyaml:2.4")
@@ -15,6 +17,15 @@ dependencies {
 
 application {
     mainClass.set("com.github.lystran.mochat.Application")
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            imageName.set("mo-chat")
+            buildArgs.add("-Ob")
+        }
+    }
 }
 
 tasks.test {
