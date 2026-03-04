f3c38cb5e534cdd2828334e3b63ad782e8cacba4
LystranG <me@lystran.com>
2026-03-04 13:44:44 +0800

    chore(protocol): export protobuf runtime api

--- protocol/build.gradle.kts
+++ protocol/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
+    `java-library`
     id("com.google.protobuf") version "0.9.5"
 }
 
 val protobufVersion = "4.30.2"
 
 dependencies {
-    implementation("com.google.protobuf:protobuf-java:$protobufVersion")
+    api("com.google.protobuf:protobuf-java:$protobufVersion")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.4")
